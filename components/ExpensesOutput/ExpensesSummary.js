f00bf92edfe0d85558216a01810d33fa6c8c6cb3
naman2807 <namanagarwaldbs438@gmail.com>
2022-08-03 10:27:49 +0530

    made components

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+function ExpensesSummary() {
+  <View>
+    <Text>Period-Last 7 days</Text>
+    <Text>$177.5</Text>
+  </View>;
+}
+
+export default ExpensesSummary;
